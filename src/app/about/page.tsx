a9c12d42a479701d81dbfafd90a5c72670c8dfc7
Okera Johnson <okerajohnson@gmail.com>
2025-08-14 22:12:15 -0400

    fakenews

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+    return (
+        <main> 
+            <section className="bg-gradient-to-b from-teal-50 to -white py-16">
+               <div className="max-w-3xl">
+               </div> 
+
+            </section>
+        </main>
+
+    )
+}
