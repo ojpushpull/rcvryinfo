362ddc4eca8c2200711eed369413c489ee34c9aa
Okera Johnson <okerajohnson@gmail.com>
2025-08-21 10:14:29 -0400

    king tut

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,7 @@
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+
+export const getStaticProps: GetStaticProps = async (context) => {};
+
+export const getStaticPaths: GetStatic
+
+export const
